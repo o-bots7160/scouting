bcd7ad4aa8293df959ffaade5a3c1c8b5151e29a
jrl2222 <jlcomputersolutions.jl@gmail.com>
2024-02-23 22:18:27 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,11 @@ import { Logo } from './Logo';
 export function Footer() {
   return (
     <footer>
+      <div className="hidden">
       <div className="mt-8 flex h-24 flex-col items-center justify-center p-2">
         <Logo />
       </div>
+        </div>
     </footer>
   );
 }
