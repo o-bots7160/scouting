edd907a0cf6a8b2fee8c5919caeb1c12cc98f69d
jrl2222 <jlcomputersolutions.jl@gmail.com>
2024-02-23 23:11:08 -0500

    Update CounterInput.tsx

--- src/components/inputs/CounterInput.tsx
+++ src/components/inputs/CounterInput.tsx
@@ -25,7 +25,8 @@ export default function CounterInput(data: CounterInputProps) {
   return (
     <div className="my-2 flex flex-row items-center justify-center">
       <button
-        className="focus:shadow-outline w-8 rounded bg-gray-500 text-2xl text-white hover:bg-red-700 focus:outline-none dark:bg-gray-700"
+        className="focus:shadow-outline w-8 rounded bg-red-rhr text-2xl text-white hover:bg-red-700 focus:outline-none dark:bg-gray-700"
+        style="width: 100px; height: 100px;"
         type="button"
         onClick={() => handleChange(-(data.step || 1))}
       >
@@ -33,7 +34,8 @@ export default function CounterInput(data: CounterInputProps) {
       </button>
       <h2 className="px-4 text-2xl dark:text-white">{data.value}</h2>
       <button
-        className="focus:shadow-outline w-8 rounded bg-gray-500 text-2xl  text-white hover:bg-red-700 focus:outline-none dark:bg-gray-700"
+        className="focus:shadow-outline w-8 rounded bg-red-rhr text-2xl  text-white hover:bg-red-700 focus:outline-none dark:bg-gray-700"
+        style="width: 100px; height: 100px;"
         type="button"
         onClick={() => handleChange(data.step || 1)}
       >
